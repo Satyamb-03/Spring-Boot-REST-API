53b7f3a2d163ba6a1a057409656c9d546e889cfc
Satyamb-03 <satyampbhatt03@gmail.com>
2024-11-12 19:22:01 +1300

    Initial commit

---
+++ src/main/java/com/example/quiztournament/QuiztournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiztournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiztournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiztournamentApplication.class, args);
+	}
+
+}
